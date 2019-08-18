ffb2fb8f5bf9f30ebf02e591c54ec9a47325fd98
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2019-08-18 10:21:44 -0400

    Implement retrieval of files from root directories

--- src/fs-site.ts
+++ src/fs-site.ts
@@ -1,4 +1,4 @@
-import { Directory, directoryHasDescendent } from "./fs-entry";
+import { Directory, directoryHasDescendent, File } from "./fs-entry";
 
 export const rootsAreMutuallyExclusive = (...roots: Directory[]): boolean => {
 	for (let i = 0; i < roots.length - 1; i++) {
@@ -11,3 +11,21 @@ export const rootsAreMutuallyExclusive = (...roots: Directory[]): boolean => {
 	}
 	return true;
 };
+
+export const filesInRoots = (
+	downwardFilesReader: (directory: Directory) => Iterable<File>,
+) =>
+	function*(...roots: Directory[]): Iterable<File> {
+		for (const root of roots) {
+			yield* downwardFilesReader(root);
+		}
+	};
+
+export const consideredFiles = (ignore: (file: File) => boolean) =>
+	function*(files: Iterable<File>): Iterable<File> {
+		for (const file of files) {
+			if (!ignore(file)) {
+				yield file;
+			}
+		}
+	};
