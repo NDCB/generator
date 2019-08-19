53eb67cc9bef3d36bdab589798b5637871c9e03c
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2019-08-19 16:11:41 -0400

    Define file data interface

--- src/fs-data.ts
+++ src/fs-data.ts
@@ -8,3 +8,10 @@ export interface Data {
 		| Data
 		| Array<null | boolean | string | number | Date | Data>;
 }
+
+export interface FileData {
+	readonly _tag: "FileData";
+	readonly value: Data;
+}
+
+export const fileDataToData = (fileData: FileData): Data => fileData.value;
