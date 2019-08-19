3d3019431a702e838692a6edc2b7f7f5b7d2c842
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2019-08-19 17:30:49 -0400

    Implement yml file data parser for generalized file data parser

--- src/yml-data.ts
+++ src/yml-data.ts
@@ -1,8 +1,20 @@
+import { Set, ValueObject } from "immutable";
 import { safeLoad } from "js-yaml";
+
 import { Data } from "./fs-data";
+import { Extension, extensionValueObjectSet } from "./fs-extension";
 import { FileContents, fileContentsToString } from "./fs-reader";
 
 export const parseYmlData = (token: string): Data => safeLoad(token);
 
 export const parseYmlFileData = (contents: FileContents): Data =>
 	parseYmlData(fileContentsToString(contents));
+
+export const ymlExtensions: Set<
+	Extension & ValueObject
+> = extensionValueObjectSet(".yml", ".yaml");
+
+export const ymlFileDataParser = {
+	extensions: ymlExtensions,
+	parser: parseYmlFileData,
+};
