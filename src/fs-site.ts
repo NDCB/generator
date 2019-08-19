d57d3bedeb5cbfc09809318740ccc9cc0d843112
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2019-08-19 13:12:22 -0400

    Implement source file retrieval

--- src/fs-site.ts
+++ src/fs-site.ts
@@ -1,14 +1,29 @@
+import flatten from "arr-flatten";
 import consola from "consola";
+import { Map, Set, ValueObject } from "immutable";
+import { extname } from "path";
 
 import {
 	Directory,
+	directory,
 	directoryHasDescendent,
 	directoryToPath,
 	directoryToString,
 	Entry,
 	entryRelativePath,
 	File,
+	file,
+	fileExists,
+	fileInDirectory,
+	fileName,
+	parentDirectory,
 } from "./fs-entry";
+import {
+	Extension,
+	extension,
+	extensionToString,
+	extensionToValueObject,
+} from "./fs-extension";
 import { joinPath, Path } from "./fs-path";
 
 export const logger = consola.withTag("fs-site");
@@ -78,4 +93,91 @@ export const pathnameFromRoot = (root: Directory) => {
 export const pathFromPathname = (root: Directory) => {
 	const join = joinPath(directoryToPath(root));
 	return (pathname: Pathname): Path => join(pathnameToString(pathname));
+};
+
+export const pathnameHasExtension = (pathname: Pathname): boolean =>
+	!!extname(pathnameToString(pathname));
+
+export const pathnameExtension = (pathname: Pathname): Extension =>
+	extension(extname(pathnameToString(pathname)));
+
+export const fileWithExtension = (file: File) => {
+	const inDirectory = fileInDirectory(parentDirectory(file));
+	const name = fileName(file);
+	return (extension: Extension): File =>
+		inDirectory(`${name}${extensionToString(extension)}`);
+};
+
+export const fileWithExtensions = (file: File) => {
+	const withExtension = fileWithExtension(file);
+	return (extensions: Set<Extension & ValueObject>): Iterable<File> =>
+		extensions.map(withExtension);
+};
+
+export const sourceExtensions = (
+	destinationToSource: Map<
+		Extension & ValueObject,
+		Set<Extension & ValueObject>
+	>,
+) => (destination: Extension) => {
+	const destinationValue = extensionToValueObject(destination);
+	return destinationToSource.get(destinationValue) || Set([destinationValue]);
+};
+
+export const possibleSourceFiles = (...roots: Directory[]) => {
+	const toPaths = roots.map(pathFromPathname);
+	return (
+		destinationToSource: Map<
+			Extension & ValueObject,
+			Set<Extension & ValueObject>
+		>,
+	) => {
+		const getSourceExtensions = sourceExtensions(destinationToSource);
+		const htmlExtension = extension(".html");
+		const htmlSourceExtensions = getSourceExtensions(htmlExtension);
+		return function*(pathname: Pathname): Iterable<File> {
+			const paths = toPaths.map((toPath) => toPath(pathname));
+			const baseFiles = paths.map(file);
+			yield* baseFiles;
+			if (pathnameHasExtension(pathname)) {
+				yield* flatten(
+					baseFiles.map((file) => [
+						...fileWithExtensions(file)(
+							getSourceExtensions(pathnameExtension(pathname)),
+						),
+					]),
+				);
+			} else {
+				yield* flatten(
+					baseFiles.map((file) => [
+						fileWithExtension(file)(htmlExtension),
+						...fileWithExtensions(file)(htmlSourceExtensions),
+					]),
+				);
+			}
+			const indexes = paths.map((path) =>
+				fileInDirectory(directory(path))("index.html"),
+			);
+			yield* indexes;
+			const indexesSource = flatten(
+				indexes.map((index) => [
+					...fileWithExtensions(index)(htmlSourceExtensions),
+				]),
+			);
+			yield* indexesSource;
+		};
+	};
+};
+
+export const sourceFile = (...roots: Directory[]) => (
+	destinationToSource: Map<
+		Extension & ValueObject,
+		Set<Extension & ValueObject>
+	>,
+) => {
+	const getPossibleSourceFiles = possibleSourceFiles(...roots)(
+		destinationToSource,
+	);
+	return (pathname: Pathname): File =>
+		[...getPossibleSourceFiles(pathname)].find(fileExists);
 };
