8610789965a534d2436bce9ffc6664d775c7b169
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2019-08-19 17:16:13 -0400

    Implement data parser merging

--- src/fs-data.ts
+++ src/fs-data.ts
@@ -1,4 +1,4 @@
-import { Map, ValueObject } from "immutable";
+import { Map, Set, ValueObject } from "immutable";
 import { File } from "./fs-entry";
 import {
 	Extension,
@@ -24,6 +24,24 @@ export interface FileData {
 }
 
 export const fileDataToData = (fileData: FileData): Data => fileData.value;
+
+export const mergeParsers = (
+	...parsers: Array<(contents: FileContents) => Data>
+) => (contents: FileContents): Data =>
+	parsers
+		.map((parser) => parser(contents))
+		.reduce((reduction, value) => ({ ...reduction, ...value }));
+
+export const handledExtensionsToParsers = (
+	handledExtensions: Set<Extension & ValueObject>,
+	parser: (contents: FileContents) => Data,
+): Map<Extension & ValueObject, (contents: FileContents) => Data> =>
+	handledExtensions.toMap().map(() => parser);
+
+export const mergeMappedParsers = (
+	parsers: Set<Map<Extension & ValueObject, (contents: FileContents) => Data>>,
+): Map<Extension & ValueObject, (contents: FileContents) => Data> =>
+	parsers.reduce((reduction, value) => reduction.merge(value));
 
 export const parseFileDataByExtension = (
 	parsers: Map<Extension & ValueObject, (contents: FileContents) => Data>,
