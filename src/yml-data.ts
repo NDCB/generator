41b7a0ca05293c04be1a3b8dc4f6036223d9ab18
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2019-08-19 17:20:03 -0400

    Fix file data parsing definition

--- src/yml-data.ts
+++ src/yml-data.ts
@@ -1,9 +1,8 @@
 import { safeLoad } from "js-yaml";
-import { FileData, fileData } from "./data.bak";
 import { Data } from "./fs-data";
 import { FileContents, fileContentsToString } from "./fs-reader";
 
 export const parseYmlData = (token: string): Data => safeLoad(token);
 
-export const parseYmlFileData = (contents: FileContents): FileData =>
-	fileData(parseYmlData(fileContentsToString(contents)));
+export const parseYmlFileData = (contents: FileContents): Data =>
+	parseYmlData(fileContentsToString(contents));
