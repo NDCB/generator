b68365ca5010dc95bae49d33f0a06e63ec83ac47
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2019-08-23 17:57:16 -0400

    Test `upwardDirectoriesFromDirectory`

---
+++ test/fs-entry.spec.ts
@@ -0,0 +1,68 @@
+import { assert } from "chai";
+
+import {
+	directory,
+	Directory,
+	directoryEquals,
+	directoryToString,
+	upwardDirectoriesFromDirectory,
+} from "../src/fs-entry";
+import { resolvedPath } from "../src/fs-path";
+
+describe("upwardDirectoriesFromDirectory", () => {
+	const asDirectory = (value: string): Directory =>
+		directory(resolvedPath(value));
+	const directoriesAsString = (directories: Directory[]): string =>
+		directories.map(directoryToString).join(";");
+	const testCases = [
+		{
+			directory: "/",
+			expected: ["/"],
+		},
+		{
+			directory: "/directory",
+			expected: ["/directory", "/"],
+		},
+		{
+			directory: "/directory/subdirectory",
+			expected: ["/directory/subdirectory", "/directory", "/"],
+		},
+	].map(({ directory, expected }) => ({
+		directory: asDirectory(directory),
+		expected: expected.map(asDirectory),
+	}));
+	for (const { directory, expected } of testCases) {
+		it(`yields "${directoriesAsString(
+			expected,
+		)}" for directory "${directoryToString(directory)}"`, () => {
+			const actual = [...upwardDirectoriesFromDirectory(directory)];
+			const displayResults = () =>
+				`Actual: "${directoriesAsString(
+					actual,
+				)}"\nExpected: "${directoriesAsString(expected)}"`;
+			assert.strictEqual(
+				actual.length,
+				expected.length,
+				`Actual and expected directories differ in length.\n${displayResults()}`,
+			);
+			for (const directory of expected) {
+				assert.isDefined(
+					actual.find((other) => directoryEquals(other, directory)),
+					`Directory "${directoryToString(
+						directory,
+					)}" is missing.\n${displayResults()}`,
+				);
+			}
+			expected
+				.map((directory, index) => [directory, actual[index]])
+				.forEach(([d1, d2]) =>
+					assert.isTrue(
+						directoryEquals(d1, d2),
+						`Directory "${directoryToString(
+							d1,
+						)}" is out of order.\n${displayResults()}`,
+					),
+				);
+		});
+	}
+});
