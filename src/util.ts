3e7953379e0b2950f9a54014c7a26d481e9238a5
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2019-08-29 09:53:25 -0400

    Implement `allPairs`

--- src/util.ts
+++ src/util.ts
@@ -1,1 +1,10 @@
+import { Seq } from "immutable";
+
 export const strictEquals = <T>(t1: T, t2: T): boolean => t1 === t2;
+
+export const allPairs = <T>(iterable: Iterable<T>): Iterable<[T, T]> => {
+	const sequence = Seq(iterable);
+	return sequence.flatMap((element1) =>
+		sequence.map((element2) => [element1, element2]),
+	);
+};
