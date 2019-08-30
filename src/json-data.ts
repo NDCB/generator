95c1837b1666e807e8455a9ea82b4eefc4761a91
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2019-08-30 08:03:25 -0400

    Implement JSON file data parser

---
+++ src/json-data.ts
@@ -0,0 +1,19 @@
+import { Set, ValueObject } from "immutable";
+
+import { Data, DataParserModule } from "./fs-data";
+import { Extension, extensionValueObjectSet } from "./fs-extension";
+import { FileContents, fileContentsToString } from "./fs-reader";
+
+export const parseJsonData = (token: string): Data => JSON.parse(token);
+
+export const parseJsonFileData = (contents: FileContents): Data =>
+	parseJsonData(fileContentsToString(contents));
+
+export const jsonExtensions: Set<
+	Extension & ValueObject
+> = extensionValueObjectSet([".json"]);
+
+export const jsonFileDataParser: DataParserModule = {
+	extensions: jsonExtensions,
+	parser: parseJsonFileData,
+};
