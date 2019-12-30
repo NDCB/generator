dbd686b2fee921674c4060ee019acd791e3e9ca0
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2019-12-30 14:00:46 -0500

    Test merging of upward categories data

--- test/category-data.spec.ts
+++ test/category-data.spec.ts
@@ -1,6 +1,16 @@
 import { assert } from "chai";
 
-import { arrayConcatenationMerger } from "../src/category-data";
+import { Map } from "immutable";
+
+import {
+	arrayConcatenationMerger,
+	keyedMerger,
+	mergedUpwardCategoriesData,
+} from "../src/category-data";
+import { Data } from "../src/fs-data";
+import { File, file, fileToString } from "../src/fs-entry";
+import { path } from "../src/fs-path";
+import { Pathname } from "../src/fs-site";
 
 describe("arrayConcatenationMerger", () => {
 	const testCases = [
@@ -22,6 +32,60 @@ describe("arrayConcatenationMerger", () => {
 			assert.deepEqual(
 				arrayConcatenationMerger(key)(reduction, current),
 				expected[key],
+			);
+		});
+	}
+});
+
+describe("mergedUpwardCategoriesData", () => {
+	const testCases: Iterable<{
+		inheritedFiles: (file: File) => (pathname: Pathname) => Iterable<File>;
+		merger: (reduction: Data, current: Data) => Data;
+		categoryFile: File;
+		properData: (file: File) => Data;
+		expected: Data;
+	}> = [
+		{
+			inheritedFiles: () => () =>
+				["category", "parent", "grandparent"].map(path).map(file),
+			merger: keyedMerger(Map()),
+			categoryFile: file(path("category")),
+			properData: (file) =>
+				({
+					grandparent: {
+						title: "grandparent",
+						color: "blue",
+						image: "banner",
+					},
+					parent: {
+						title: "parent",
+						image: "image",
+					},
+					category: {
+						title: "category",
+					},
+				}[fileToString(file)]),
+			expected: {
+				title: "category",
+				color: "blue",
+				image: "image",
+			},
+		},
+	];
+	for (const {
+		inheritedFiles,
+		merger,
+		categoryFile,
+		properData,
+		expected,
+	} of testCases) {
+		it("merges the data from the uppermost category to the inheritor", () => {
+			assert.deepEqual(
+				mergedUpwardCategoriesData(inheritedFiles, merger)(
+					categoryFile,
+					properData,
+				),
+				expected,
 			);
 		});
 	}
