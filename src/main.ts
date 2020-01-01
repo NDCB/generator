7d8889c7b8bab6de4ad01b380ae51afc6c18b485
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2020-01-01 18:39:35 -0500

    Add command descriptions

--- src/main.ts
+++ src/main.ts
@@ -16,3 +16,31 @@ program.version(
 		),
 	).version as string,
 );
+
+const currentWorkingDirectory = directory(path(process.cwd()));
+
+program
+	.command("server [sources...]")
+	.option("-h, --hostname <hostname>", "specify the server's hostname")
+	.option("-p, --port <port>", "specify the server's port")
+	.usage("[source root directories...] --hostname localhost --port 3000")
+	.description(
+		"Run a server at the given hostname and port number, " +
+			"serving built files sourced from the given root directories",
+	)
+	.action((sources = [], { hostname = "localhost", port = 3000 }) => {});
+
+program
+	.command("build [sources...]")
+	.option("-d, --destination <destination>", "specify the build destination")
+	.usage("[source root directories...] --destination ./www")
+	.description(
+		"Build the website using the files sourced from the given root directories",
+	)
+	.action((sources = [], { destination = "./www" }) => {});
+
+program.parse(process.argv);
+
+if (program.args.length < 1) {
+	program.help();
+}
