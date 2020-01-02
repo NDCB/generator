f20ff2f0a26d475713bc179d08c84bf4cb8f5fd7
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2020-01-02 18:40:38 -0500

    Base implementation of sequential build task

--- src/build.ts
+++ src/build.ts
@@ -1,6 +1,10 @@
-import { Set, ValueObject } from "immutable";
+import consola from "consola";
+import { Seq, Set, ValueObject } from "immutable";
 
-import { Directory } from "./fs-entry";
+import { Directory, emptyDirectory, File, fileToString } from "./fs-entry";
+import { fileFromPathname } from "./fs-site";
+
+export const logger = consola.withTag("build");
 
 export const build = (
 	roots: Set<Directory & ValueObject>,
@@ -8,3 +12,27 @@ export const build = (
 ): void => {
 	throw new Error("Not implemented yet");
 };
+
+export const sequentialBuild = (builder: (file: File) => void) => (
+	sourceFiles: Iterable<File>,
+): void => {
+	Seq(sourceFiles).forEach(builder);
+};
+
+export const logBuilder = (builder: (file: File) => void) => (
+	file: File,
+): void => {
+	const stringOfFile = fileToString(file);
+	logger.info(`Building file "${stringOfFile}"`);
+	try {
+		builder(file);
+		logger.success(`Successfully built file "${stringOfFile}"`);
+	} catch (error) {
+		logger.error(`Failed to build file "${stringOfFile}"`);
+		throw error;
+	}
+};
+
+export const destinationFileFromPathname = fileFromPathname;
+
+export const emptyDestination = emptyDirectory;
