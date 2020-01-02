0c2072833f053f92488d5db973e57fa1d65c7280
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2020-01-02 10:45:04 -0500

    Revise definition of a document

--- src/processor.ts
+++ src/processor.ts
@@ -1,7 +1,7 @@
-import { hash, Set, ValueObject } from "immutable";
+import { hash, ValueObject } from "immutable";
 import { lookup as mimeType } from "mime-types";
 
-import { Extension, extensionEquals } from "./fs-extension";
+import { Extension } from "./fs-extension";
 import { FileContents } from "./fs-reader";
 import { Pathname, pathnameExtension, pathnameToString } from "./fs-site";
 import { strictEquals } from "./util";
@@ -37,21 +37,16 @@ export const documentTypeToValueObject = (
 export interface Document {
 	readonly _tag: "Document";
 	readonly contents: FileContents;
-	readonly type: DocumentType;
 	readonly location: Pathname;
 }
 
 export const document = (
 	contents: FileContents,
-	type: DocumentType,
 	location: Pathname,
-): Document => ({ _tag: "Document", contents, type, location });
+): Document => ({ _tag: "Document", contents, location });
 
 export const documentContents = (document: Document): FileContents =>
 	document.contents;
-
-export const documentTypeFromDocument = (document: Document): DocumentType =>
-	document.type;
 
 export const documentLocation = (document: Document): Pathname =>
 	document.location;
@@ -61,37 +56,3 @@ export const documentExtension = (document: Document): Extension =>
 
 export const documentMimeType = (document: Document): string =>
 	mimeType(pathnameToString(documentLocation(document))) || "text/plain";
-
-export interface DocumentProcessor {
-	readonly handlesDocument: (document: Document) => boolean;
-	readonly processDocument: (document: Document) => Document;
-}
-
-export const handleDocumentOfType = (type: DocumentType) => (
-	document: Document,
-): boolean => documentTypeEquals(type, documentTypeFromDocument(document));
-
-export const handleDocumentOfTypes = (
-	types: Set<DocumentType & ValueObject>,
-) => {
-	const handledByType = types.map(handleDocumentOfType);
-	return (document: Document): boolean =>
-		handledByType.some((handles) => handles(document));
-};
-
-export const handleDocumentOfExtension = (extension: Extension) => (
-	document: Document,
-): boolean => extensionEquals(extension, documentExtension(document));
-
-export const handleDocumentOfExtensions = (
-	extensions: Set<Extension & ValueObject>,
-) => {
-	const handleByExtension = extensions.map(handleDocumentOfExtension);
-	return (document: Document): boolean =>
-		handleByExtension.some((handles) => handles(document));
-};
-
-export const compositeHandleRule = (
-	...rules: Array<(document: Document) => boolean>
-) => (document: Document): boolean =>
-	rules.some((handles) => handles(document));
