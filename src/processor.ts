0ba7abae1e57e1de84b44d80c4a7df8be8a763d0
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2020-01-03 17:31:03 -0500

    Move `DocumentType` to relational data

--- src/processor.ts
+++ src/processor.ts
@@ -1,36 +1,5 @@
-import { hash, ValueObject } from "immutable";
-
 import { FileContents } from "./fs-reader";
 import { Pathname } from "./fs-site";
-import { strictEquals } from "./util";
-
-export interface DocumentType {
-	readonly _tag: "DocumentType";
-	readonly value: string;
-}
-
-export const documentType = (value: string): DocumentType => ({
-	_tag: "DocumentType",
-	value,
-});
-
-export const documentTypeToString = (type: DocumentType): string => type.value;
-
-export const documentTypeEquals = (
-	t1: DocumentType,
-	t2: DocumentType,
-): boolean => strictEquals(documentTypeToString(t1), documentTypeToString(t2));
-
-export const isDocumentType = (element: any): element is DocumentType =>
-	!!element && strictEquals(element._tag, "DocumentType");
-
-export const documentTypeToValueObject = (
-	type: DocumentType,
-): DocumentType & ValueObject => ({
-	...type,
-	equals: (other) => isDocumentType(other) && documentTypeEquals(type, other),
-	hashCode: () => hash(documentTypeToString(type)),
-});
 
 export interface Document {
 	readonly _tag: "Document";
--- src/relational-data.ts
+++ src/relational-data.ts
@@ -1,8 +1,36 @@
-import { Map, ValueObject } from "immutable";
+import { hash, Map, ValueObject } from "immutable";
 
 import { Data } from "./fs-data";
 import { File } from "./fs-entry";
-import { DocumentType, documentTypeToValueObject } from "./processor";
+import { strictEquals } from "./util";
+
+export interface DocumentType {
+	readonly _tag: "DocumentType";
+	readonly value: string;
+}
+
+export const documentType = (value: string): DocumentType => ({
+	_tag: "DocumentType",
+	value,
+});
+
+export const documentTypeToString = (type: DocumentType): string => type.value;
+
+export const documentTypeEquals = (
+	t1: DocumentType,
+	t2: DocumentType,
+): boolean => strictEquals(documentTypeToString(t1), documentTypeToString(t2));
+
+export const isDocumentType = (element: any): element is DocumentType =>
+	!!element && strictEquals(element._tag, "DocumentType");
+
+export const documentTypeToValueObject = (
+	type: DocumentType,
+): DocumentType & ValueObject => ({
+	...type,
+	equals: (other) => isDocumentType(other) && documentTypeEquals(type, other),
+	hashCode: () => hash(documentTypeToString(type)),
+});
 
 export const relationalData = (
 	dataFetchers: Map<
