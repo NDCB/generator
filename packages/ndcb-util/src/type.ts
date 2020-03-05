ba707e762262c22bc07315c56113a6bd95ced8b5
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2020-03-05 18:05:26 -0500

    feat(util): implement string type assertion

--- packages/ndcb-util/src/type.ts
+++ packages/ndcb-util/src/type.ts
@@ -1,2 +1,5 @@
+export const isString = (element: unknown): element is string =>
+	typeof element === "string";
+
 export const isNumber = (element: unknown): element is number =>
 	typeof element === "number";
--- packages/ndcb-util/test/type.spec.ts
+++ packages/ndcb-util/test/type.spec.ts
@@ -1,4 +1,12 @@
-import { isNumber } from "../src/type";
+import { isNumber, isString } from "../src/type";
+
+describe("isString", () => {
+	for (const { input, expected } of require("./fixtures/isString.json")) {
+		test(`returns "${expected}" for input "${input}"`, () => {
+			expect(isString(input)).toBe(expected);
+		});
+	}
+});
 
 describe("isNumber", () => {
 	for (const { input, expected } of require("./fixtures/isNumber.json")) {
