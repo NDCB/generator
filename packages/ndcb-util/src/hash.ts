67a255a109b4cc4763134159446abde850ebbd4c
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2020-03-18 10:37:44 -0400

    feat(util): implement string hashing

---
+++ packages/ndcb-util/src/hash.ts
@@ -0,0 +1,9 @@
+export const hashString = (value: string): number => {
+	let hash = 0;
+	if (value.length === 0) return hash;
+	for (let i = 0; i < value.length; i++) {
+		hash = (hash << 5) - hash + value.charCodeAt(i);
+		hash |= 0;
+	}
+	return hash;
+};
