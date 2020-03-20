5f34f2fb4c631c37459ae7ba27ddca4a3b274ee3
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2020-03-20 16:33:57 -0400

    feat(fs): implement `Extension` data type

---
+++ packages/ndcb-fs/src/extension.ts
@@ -0,0 +1,23 @@
+import { hashString } from "@ndcb/util";
+
+export interface Extension {
+	readonly _tag: "Extension";
+	readonly value: string;
+}
+
+export const extensionToString = (extension: Extension): string =>
+	extension.value;
+
+export const extensionEquals = (e1: Extension, e2: Extension): boolean =>
+	e1.value === e2.value;
+
+export const hashExtension = (extension: Extension): number =>
+	hashString(extensionToString(extension));
+
+export const extension = (value: string): Extension => ({
+	_tag: "Extension",
+	value,
+});
+
+export const isExtension = (element: any): element is Extension =>
+	!!element && element._tag === "Extension";
