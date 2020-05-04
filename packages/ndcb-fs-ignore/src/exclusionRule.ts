69c1e1a5b45fecadd67142bc0f5a34ff40d569f0
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2020-05-04 09:00:05 -0400

    feat(fs-ignore): implement exclusion rule predicate type and composite exclusion rule

---
+++ packages/ndcb-fs-ignore/src/exclusionRule.ts
@@ -0,0 +1,11 @@
+import { File } from "@ndcb/fs";
+import { some } from "@ndcb/util";
+
+export type ExclusionRule = (file: File) => boolean;
+
+export const compositeExclusionRule = (
+  rules: Iterable<ExclusionRule>,
+): ExclusionRule => {
+  rules = [...rules];
+  return (file: File): boolean => some(rules, (applies) => applies(file));
+};
