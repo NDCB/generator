b63f4d2042b85a043528d140fd618ac00556b4e7
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2020-07-10 00:16:42 -0400

    feat(mock-fs): implement file system mocking utility

---
+++ packages/ndcb-mock-fs/README.md
@@ -0,0 +1,10 @@
+[![NPM version][npm-shield]][npm-url]
+[![MIT License][license-shield]][license-url]
+
+# @ndcb/mock-fs
+
+[npm-shield]: https://img.shields.io/npm/v/@ndcb/mock-fs.svg
+[npm-url]: https://www.npmjs.com/package/@ndcb/mock-fs
+
+[license-shield]: https://img.shields.io/github/license/NDCB/generator.svg?style=flat
+[license-url]: ./LICENSE.md
---
+++ packages/ndcb-mock-fs/src/mock.ts
@@ -0,0 +1,104 @@
+import {
+  File,
+  Directory,
+  FileReader,
+  DirectoryReader,
+  Entry,
+  normalizedFile,
+  normalizedDirectory,
+  absolutePathToString,
+  normalizedAbsolutePath,
+  fileEquals,
+  directoryEquals,
+  fileToPath,
+  fileContents,
+  directoryToPath,
+  fileToString,
+  directoryToString,
+} from "@ndcb/fs-util";
+import { some } from "@ndcb/util";
+
+export type MockFile = string;
+
+export interface MockDirectory {
+  readonly [key: string]: MockFile | MockDirectory;
+}
+
+export type MockEntry = MockFile | MockDirectory;
+
+export interface MockFs {
+  readonly "/": MockDirectory;
+}
+
+const mockEntryIsFile = (entry: MockEntry): entry is MockFile =>
+  typeof entry === "string";
+
+const mockEntryIsDirectory = (entry: MockEntry): entry is MockDirectory =>
+  typeof entry === "object";
+
+export const mockFileSystem = (
+  structure: MockFs,
+): {
+  fileExists: (file: File) => boolean;
+  directoryExists: (directory: Directory) => boolean;
+  readFile: FileReader;
+  readDirectory: DirectoryReader;
+} => {
+  const mockFiles: File[] = [];
+  const mockDirectories: Directory[] = [];
+  const mockFileContents: Map<string, string> = new Map();
+  const mockDirectoryContents: Map<string, Entry[]> = new Map();
+  const traverse = (root: MockDirectory, base: string): void => {
+    for (const segment in root) {
+      const entry: MockEntry = root[segment];
+      if (mockEntryIsFile(entry)) {
+        mockFiles.push(normalizedFile(base + segment));
+        mockFileContents.set(
+          absolutePathToString(normalizedAbsolutePath(base + segment)),
+          entry,
+        );
+      } else if (mockEntryIsDirectory(entry)) {
+        mockDirectories.push(normalizedDirectory(base + segment));
+        const directoryBase = base + segment + "/";
+        const entries: Entry[] = [];
+        for (const subsegment in entry) {
+          if (mockEntryIsFile(entry[subsegment])) {
+            entries.push(normalizedFile(directoryBase + subsegment));
+          } else if (mockEntryIsDirectory(entry[subsegment])) {
+            entries.push(normalizedDirectory(directoryBase + subsegment));
+          }
+        }
+        mockDirectoryContents.set(
+          absolutePathToString(normalizedAbsolutePath(base + segment)),
+          entries,
+        );
+        traverse(entry, directoryBase);
+      }
+    }
+  };
+  traverse(structure["/"], "/");
+  return {
+    fileExists: (file: File) =>
+      some(mockFiles, (mock) => fileEquals(file, mock)),
+    directoryExists: (directory: Directory) =>
+      some(mockDirectories, (mock) => directoryEquals(directory, mock)),
+    readFile: (file: File) => {
+      const contents = mockFileContents.get(
+        absolutePathToString(fileToPath(file)),
+      );
+      if (contents === undefined)
+        throw new Error(`File does not exist "${fileToString(file)}"`);
+      return fileContents(contents);
+    },
+    readDirectory: (directory: Directory) => {
+      const contents = mockDirectoryContents.get(
+        absolutePathToString(directoryToPath(directory)),
+      );
+      if (contents === undefined)
+        throw new Error(
+          `Directory does not exist "${directoryToString(directory)}"`,
+        );
+      return contents;
+    },
+  };
+};
