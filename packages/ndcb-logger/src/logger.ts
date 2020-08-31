ef78db30bea785a42f6e3a31dc19d7a8152b38bc
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2020-08-30 22:54:21 -0400

    refactor(logger): move implementation out of index file

---
+++ packages/ndcb-logger/src/logger.ts
@@ -0,0 +1,50 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const consola = require("consola");
+
+import { IO } from "@ndcb/util";
+
+export type MessageLogger = (message: string) => IO<void>;
+
+const messageLogger = (logger: (message: string) => void): MessageLogger => (
+  message: string,
+) => () => logger(message);
+
+export interface Logger {
+  readonly fatal: MessageLogger;
+  readonly error: MessageLogger;
+  readonly warn: MessageLogger;
+  readonly log: MessageLogger;
+  readonly info: MessageLogger;
+  readonly start: MessageLogger;
+  readonly success: MessageLogger;
+  readonly ready: MessageLogger;
+  readonly debug: MessageLogger;
+  readonly trace: MessageLogger;
+}
+
+export const scoppedLogger = (scope: string): Logger => {
+  const {
+    fatal,
+    error,
+    warn,
+    log,
+    info,
+    start,
+    success,
+    ready,
+    debug,
+    trace,
+  } = consola.create({ scope, level: Infinity });
+  return {
+    fatal: messageLogger(fatal),
+    error: messageLogger(error),
+    warn: messageLogger(warn),
+    log: messageLogger(log),
+    info: messageLogger(info),
+    start: messageLogger(start),
+    success: messageLogger(success),
+    ready: messageLogger(ready),
+    debug: messageLogger(debug),
+    trace: messageLogger(trace),
+  };
+};
