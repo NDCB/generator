32dcdf737fee4f2d38fded7999b19791d06200a1
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2020-08-31 23:56:59 -0400

    feat(util): implement `Either` sequencing

--- packages/ndcb-util/src/either.ts
+++ packages/ndcb-util/src/either.ts
@@ -114,3 +114,10 @@ export const iterableIsAllRight = <L, R>(
   iterable: Iterable<Either<L, R>>,
 ): iterable is Iterable<Right<R>> =>
   every(iterable, (element) => eitherIsRight(element));
+
+export const sequence = <L, R>(
+  eithers: readonly Either<L, R>[],
+): Either<L, readonly R[]> =>
+  eithers.some(eitherIsLeft)
+    ? left(eitherValue(eithers.find(eitherIsLeft) as Left<L>))
+    : right(eithers.map((either) => eitherValue(either as Right<R>)) as R[]);
