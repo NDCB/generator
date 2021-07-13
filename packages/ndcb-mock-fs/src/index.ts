43abe083bf24c65fab393246b5719bffdfc06ca1
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2021-07-13 17:25:57 -0400

    refactor(mock-fs): refactor modules and revise tests to use `test.each`

--- packages/ndcb-mock-fs/src/index.ts
+++ packages/ndcb-mock-fs/src/index.ts
@@ -1,1 +1,154 @@
-export * from "./mock.js";
+import { io, taskEither, function as fn } from "fp-ts";
+import type { IO } from "fp-ts/IO";
+import type { TaskEither } from "fp-ts/TaskEither";
+
+import upath from "upath";
+
+import { file, directory, entry } from "@ndcb/fs-util";
+import type {
+  File,
+  Directory,
+  FileReader,
+  Entry,
+  DirectoryReader,
+} from "@ndcb/fs-util";
+import { hashMap, sequence } from "@ndcb/util";
+import type { HashMap, Sequence } from "@ndcb/util";
+
+export type MockFile = string;
+
+export interface MockDirectory {
+  readonly [key: string]: MockFile | MockDirectory;
+}
+
+export type MockEntry = MockFile | MockDirectory;
+
+const mockEntryIsFile = (entry: MockEntry): entry is MockFile =>
+  typeof entry === "string";
+
+const mockEntryIsDirectory = (entry: MockEntry): entry is MockDirectory =>
+  typeof entry === "object";
+
+/* istanbul ignore next: only occurs when the library is misused */
+const mockEntryPatternMatchingError = (entry: unknown): Error =>
+  new Error(
+    `Failed <MockEntry> pattern matching for object "${JSON.stringify(entry)}"`,
+  );
+
+const mockDirectoryEntries = function* (
+  dir: MockDirectory,
+  base: string,
+): Sequence<Entry> {
+  for (const segment in dir) {
+    const entry = dir[segment];
+    const entryPathString = upath.join(base, segment);
+    /* istanbul ignore else: unexpected branch */
+    if (mockEntryIsFile(entry)) yield file.makeNormalized(entryPathString);
+    else if (mockEntryIsDirectory(entry))
+      yield directory.makeNormalized(entryPathString);
+    else throw mockEntryPatternMatchingError(entry);
+  }
+};
+
+const mockEntries = function* (
+  structure: MockDirectory,
+): Sequence<[File, string] | [Directory, Entry[]]> {
+  const traverse = function* (
+    root: MockDirectory,
+    base: string,
+  ): Sequence<[File, string] | [Directory, Entry[]]> {
+    for (const segment in root) {
+      const entry: MockEntry = root[segment];
+      /* istanbul ignore else: unexpected branch */
+      if (mockEntryIsFile(entry)) {
+        yield [file.makeNormalized(upath.join(base, segment)), entry];
+      } else if (mockEntryIsDirectory(entry)) {
+        const directoryBase = upath.join(base, segment);
+        const dir = directory.makeNormalized(directoryBase);
+        const subentries: Entry[] = [
+          ...mockDirectoryEntries(entry, directoryBase),
+        ];
+        yield [dir, subentries];
+        yield* traverse(entry, directoryBase);
+      } else throw mockEntryPatternMatchingError(entry);
+    }
+  };
+  yield* traverse(structure, "");
+};
+
+const fileHashMap = <T>(entries: Sequence<[File, T]>): HashMap<File, T> =>
+  hashMap.hashMap(entries, file.hash, file.Eq);
+
+const directoryHashMap = <T>(
+  entries: Sequence<[Directory, T]>,
+): HashMap<Directory, T> =>
+  hashMap.hashMap(entries, directory.hash, directory.Eq);
+
+interface TransformedMockStructure {
+  readonly fileContents: HashMap<File, string>;
+  readonly directoryEntries: HashMap<Directory, Entry[]>;
+}
+
+const transformMockStructure = (
+  structure: MockDirectory,
+): TransformedMockStructure => {
+  const entries = [...mockEntries(structure)];
+  const mockIsFile = (mock: [Entry, unknown]): mock is [File, string] =>
+    entry.isFile(mock[0]);
+  const mockIsDirectory = (
+    mock: [Entry, unknown],
+  ): mock is [Directory, Entry[]] => entry.isDirectory(mock[0]);
+  return {
+    fileContents: fileHashMap(fn.pipe(entries, sequence.filter(mockIsFile))),
+    directoryEntries: directoryHashMap(
+      fn.pipe(entries, sequence.filter(mockIsDirectory)),
+    ),
+  };
+};
+
+export interface MockFileReadError extends Error {
+  readonly file: File;
+}
+
+export interface MockDirectoryReadError extends Error {
+  readonly directory: Directory;
+}
+
+export interface MockFileSystem {
+  fileExists: (file: File) => IO<TaskEither<never, boolean>>;
+  directoryExists: (directory: Directory) => IO<TaskEither<never, boolean>>;
+  readFile: FileReader<MockFileReadError>;
+  readDirectory: DirectoryReader<MockDirectoryReadError>;
+}
+
+export const make: (structure: MockDirectory) => MockFileSystem = fn.flow(
+  transformMockStructure,
+  ({ fileContents, directoryEntries }) => ({
+    fileExists: (file) => io.of(taskEither.right(fileContents.has(file))),
+    directoryExists: (directory) =>
+      io.of(taskEither.right(directoryEntries.has(directory))),
+    readFile: (f) =>
+      fn.pipe(
+        f,
+        fileContents.get,
+        taskEither.fromOption(() => ({
+          name: "MOCKED_FILE_NOT_FOUND",
+          message: `Failed to read mocked file "${file.toString(f)}"`,
+          file: f,
+        })),
+        taskEither.map((contents) => Buffer.from(contents)),
+        io.of,
+      ),
+    readDirectory: (d: Directory) =>
+      fn.pipe(
+        d,
+        directoryEntries.get,
+        taskEither.fromOption(() => ({
+          name: "MOCKED_DIRECTORY_NOT_FOUND",
+          message: `Failed to read mocked directory "${directory.toString(d)}"`,
+          directory: d,
+        })),
+        io.of,
+      ),
+  }),
+);
---
+++ packages/ndcb-mock-fs/test/index.spec.ts
@@ -0,0 +1,284 @@
+import { describe, expect, test } from "@jest/globals";
+
+import {
+  io,
+  task,
+  taskEither,
+  readonlyArray,
+  function as fn,
+  readonlySet,
+} from "fp-ts";
+
+import { file, directory, entry } from "@ndcb/fs-util";
+import type { File, Directory, Entry } from "@ndcb/fs-util";
+
+import * as _ from "@ndcb/mock-fs";
+
+describe("make(...)#fileExists", () => {
+  const { fileExists } = _.make({
+    "/": {
+      "file.txt": "",
+      directory: {
+        "file.txt": "",
+      },
+    },
+  });
+  const fileExistsThrow = (f: File) =>
+    fn.pipe(
+      f,
+      fileExists,
+      io.map(
+        taskEither.getOrElse(() => {
+          throw new Error(
+            `Unexpectedly failed to test for the existence of file "${file.toString(
+              f,
+            )}"`,
+          );
+        }),
+      ),
+    );
+  test.concurrent.each(
+    ["/file.txt", "/directory/file.txt"].map(file.makeNormalized),
+  )(`returns true for existent files`, async (file: File) => {
+    expect(await fn.pipe(file, fileExistsThrow)()()).toBe(true);
+  });
+  test.concurrent.each(
+    [
+      "/inexistent.txt",
+      "/directory/inexistent.txt",
+      "/inexistent/inexistent.txt",
+      "/inexistent",
+    ].map(file.makeNormalized),
+  )(`returns false for inexistent files`, async (file: File) => {
+    expect(await fn.pipe(file, fileExistsThrow)()()).toBe(false);
+  });
+});
+
+describe("make(...)#directoryExists", () => {
+  const { directoryExists } = _.make({
+    "/": {
+      "file.txt": "",
+      directory: {
+        "file.txt": "",
+        subdirectory: {},
+      },
+    },
+  });
+  const directoryExistsThrow = (d: Directory) =>
+    fn.pipe(
+      d,
+      directoryExists,
+      io.map(
+        taskEither.getOrElse(() => {
+          throw new Error(
+            `Unexpectedly failed to test for the existence of directory "${directory.toString(
+              d,
+            )}"`,
+          );
+        }),
+      ),
+    );
+  test.concurrent.each(
+    ["/", "/directory", "/directory/subdirectory"].map(
+      directory.makeNormalized,
+    ),
+  )(`returns true for existent directories`, async (directory: Directory) => {
+    expect(await fn.pipe(directory, directoryExistsThrow)()()).toBe(true);
+  });
+  test.concurrent.each(
+    [
+      "/inexistent",
+      "/directory/inexistent",
+      "/inexistent/inexistent",
+      "/file.txt",
+    ].map(directory.makeNormalized),
+  )(
+    `returns false for inexistent directories`,
+    async (directory: Directory) => {
+      expect(await fn.pipe(directory, directoryExistsThrow)()()).toBe(false);
+    },
+  );
+});
+
+describe("make(...)#readFile", () => {
+  const { readFile } = _.make({
+    "/": {
+      "file.txt": "Root file",
+      directory: {
+        "file.txt": "Nested file",
+      },
+    },
+  });
+  test.concurrent.each(["/inexistent.txt"].map(file.makeNormalized))(
+    "returns an error for inexistent files",
+    async (f: File) => {
+      expect(
+        await fn.pipe(
+          f,
+          readFile,
+          io.map(
+            fn.flow(
+              taskEither.swap,
+              taskEither.getOrElse(() => {
+                throw new Error(
+                  `Unexpectedly succeeded in reading file "${file.toString(
+                    f,
+                  )}"`,
+                );
+              }),
+              task.map(fn.constTrue),
+            ),
+          ),
+        )()(),
+      ).toBe(true);
+    },
+  );
+  test.concurrent.each(
+    [
+      {
+        file: "/file.txt",
+        contents: "Root file",
+      },
+      {
+        file: "/directory/file.txt",
+        contents: "Nested file",
+      },
+    ].map(({ file: f, contents }): [File, Buffer] => [
+      file.makeNormalized(f),
+      Buffer.from(contents),
+    ]),
+  )(
+    "reads the contents of existing files",
+    async (f: File, contents: Buffer) => {
+      expect(
+        await fn.pipe(
+          f,
+          readFile,
+          io.map(
+            taskEither.getOrElse(() => {
+              throw new Error(
+                `Unexpectedly failed to read file "${file.toString(f)}"`,
+              );
+            }),
+          ),
+        )()(),
+      ).toEqual(contents);
+    },
+  );
+});
+
+describe("make(...)#readDirectory", () => {
+  const { readDirectory } = _.make({
+    "/": {
+      "file.txt": "Root file",
+      directory: {
+        "file.txt": "Nested file",
+        subdirectory: {},
+      },
+    },
+  });
+  test.concurrent.each(["/inexistent"].map(directory.makeNormalized))(
+    "returns an error for inexistent directories",
+    async (d: Directory) => {
+      expect(
+        await fn.pipe(
+          d,
+          readDirectory,
+          io.map(
+            fn.flow(
+              taskEither.swap,
+              taskEither.getOrElse(() => {
+                throw new Error(
+                  `Unexpectedly succeeded to read directory "${directory.toString(
+                    d,
+                  )}"`,
+                );
+              }),
+              task.map(fn.constTrue),
+            ),
+          ),
+        )()(),
+      ).toBe(true);
+    },
+  );
+  test.concurrent.each(
+    [
+      {
+        directory: "/",
+        contents: [
+          {
+            type: "file",
+            path: "/file.txt",
+          },
+          {
+            type: "directory",
+            path: "/directory",
+          },
+        ],
+      },
+      {
+        directory: "/directory",
+        contents: [
+          {
+            type: "file",
+            path: "/directory/file.txt",
+          },
+          {
+            type: "directory",
+            path: "/directory/subdirectory",
+          },
+        ],
+      },
+      {
+        directory: "/directory/subdirectory",
+        contents: [],
+      },
+    ].map(({ directory: d, contents }): [Directory, ReadonlySet<Entry>] => [
+      directory.makeNormalized(d),
+      fn.pipe(
+        contents,
+        readonlyArray.map(({ type, path }) => {
+          switch (type) {
+            case "file":
+              return file.makeNormalized(path);
+            case "directory":
+              return directory.makeNormalized(path);
+            default:
+              throw new Error(
+                `Unexpected <"file" | "directory"> pattern matching error for object "${JSON.stringify(
+                  type,
+                )}"`,
+              );
+          }
+        }),
+        readonlySet.fromReadonlyArray(entry.Eq),
+      ),
+    ]),
+  )(
+    "reads the contents of existing directories",
+    async (d: Directory, expected: ReadonlySet<Entry>) => {
+      expect(
+        await fn.pipe(
+          d,
+          readDirectory,
+          io.map(
+            fn.flow(
+              taskEither.getOrElse(() => {
+                throw new Error(
+                  `Unexpectedly failed to read directory "${directory.toString(
+                    d,
+                  )}"`,
+                );
+              }),
+              task.map(
+                fn.flow(readonlySet.fromReadonlyArray(entry.Eq), (actual) =>
+                  readonlySet.getEq(entry.Eq).equals(actual, expected),
+                ),
+              ),
+            ),
+          ),
+        )()(),
+      ).toEqual(true);
+    },
+  );
+});
